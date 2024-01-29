4b0ea938cffdf5527c77972ec4b68bfc48228c27
Talisson Nunes <talisson.nunes@solides.com.br>
2024-01-29 09:12:12 -0300

    include http assertions

--- httprequestscriptexample/httprequestscriptexample.js
+++ httprequestscriptexample/httprequestscriptexample.js
@@ -1,7 +1,13 @@
 import http from "k6/http";
+import { check } from "k6";
 
 export default function () {
   const res = http.get("https://test.k6.io");
-
-  console.log(res.status);
+  check(res, {
+    "requisition status code is 200": (r) => r.status === 200,
+    "the request page is startpage": (r) =>
+      r.body.includes(
+        "Collection of simple web-pages suitable for load testing."
+      ),
+  });
 }
